a66b7f95e359b8ef8e54feab5ebec8a661a14289
VishnuGanapathi <13vishnu1996@gmail.com>
2017-07-09 14:40:28 +0530

    Create Alphabet.java

---
+++ Alphabet.java
@@ -0,0 +1,19 @@
+package com.toy.anagrams.lib;
+
+import java.util.Scanner;
+
+public class Epsilon
+{
+    public static void main ( String[] args){
+  System.out.println("Please enter a single character: ");
+  String character = in.next();
+
+  System.out.println(character);
+
+  if (character.isLetter()){
+    System.out.println("The character entered is a letter.");
+  }
+  else (character.isDigit()){
+    Syste.out.println("The character entered is a digit.");
+}
+}
